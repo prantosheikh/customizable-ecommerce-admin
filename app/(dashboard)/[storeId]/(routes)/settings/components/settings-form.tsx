17bc6843b66794282fa4cb0c2880d5316e14623f
Pranto sheikh <prantosheikh834@gmail.com>
2024-03-26 13:33:27 +0600

    fainal commit

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -22,7 +22,7 @@ import {
 import { Input } from "@/components/ui/input";
 import toast from "react-hot-toast";
 import axios from "axios";
-import { AlertModal } from "@/components/modals/Alert-modal";
+import { AlertModal } from "@/components/modals/alert-modal";
 import { ApiAlert } from "@/components/ui/api-alert";
 import { useOrigin } from "@/hooks/use-origin";
 
